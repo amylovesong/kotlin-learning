0a100abf292a31e31590e289c0718f1d13ffff2f
amylovesong <amylovesong.sun@gmail.com>
2020-05-01 23:58:25 +0800

    Advanced lesson 2: Lambda

---
+++ app/src/main/java/com/sun/kotlinlearning/LambdaSample.kt
@@ -0,0 +1,83 @@
+package com.sun.kotlinlearning
+
+import android.view.View
+import android.widget.TextView
+
+/**
+ *
+ * @author sxl (sunxiaoling@didiglobal.com)
+ * @since 2020/5/1.
+ */
+class LambdaSample {
+    fun b(param: Int): String {
+        val result = param * 2
+        return "$result"
+    }
+
+    fun a(f: (Int) -> String) {
+        f(2)
+    }
+
+    fun methodSample() {
+        // ① 函数类型
+        // 函数类型的使用：注意前面的双冒号。双冒号使函数变成了(函数类型的)对象
+        a(::b)
+        val d = ::b
+
+        // 对函数 b 的使用
+        b(1)
+        d(1)
+        (::b)(1)
+
+        // 函数类型的对象有一个 invoke() 方法
+        d(2) // 实际上会调用 d.invoke(2)
+
+        // ② 匿名函数
+        a(fun(param: Int): String {
+            return param.toString()
+        })
+        val f =  fun(param: Int): String {
+            val result = param * 10
+            return "$result"
+        }
+
+        fun setOnClickListener(onClick: (View) -> Unit) {
+            println(onClick)
+            // ...
+        }
+
+        setOnClickListener(fun(v: View): Unit{
+            println(v)
+        })
+
+        // 再简化一点的写法：lambda 表达式
+        setOnClickListener({ v: View ->
+            println(v)
+        })
+
+        // ③ lambda（变形）
+        // -- 1. lambda 是函数的最后一个参数：写在圆括号外
+        setOnClickListener() { v: View ->
+            println(v)
+        }
+        // -- 2. lambda 是函数的唯一参数：省略圆括号()
+        setOnClickListener { v: View ->
+            println(v)
+        }
+        // -- 3. lambda 是单参数的：省略参数
+        setOnClickListener {
+            println(it)
+        }
+        // -- 4. 给变量赋值时，不能省略参数类型(因为没有足够的上下文来推导参数类型)
+        val fc = { param: Int ->
+            // lambda 的最后一行代码就是它的返回值
+            param.toString()
+
+            // return 会直接结束外层函数（methodSample）
+//            return param.toString()
+        }
+
+
+    }
+
+}
--- notes/Notes-2.md
+++ notes/Notes-2.md
@@ -51,3 +51,48 @@ inline fun <reified T> printIfTypeMatch(item: Any) {
 
 ### Lesson 2
 > [Kotlin 的 Lambda 表达式](https://kaixue.io/kotlin-lambda/)
+
+##### 高阶函数
+函数类型：由「参数类型 + 返回值类型」来确定。  
+在 Java 中不存在，是 Kotlin 中新加入的一种类型。
+
+参数/返回值是「函数类型」的函数：高阶函数（Higher-Order Functions）。
+
+只是一类函数的称呼，没有特别的功能。
+
+##### 函数类型的使用
+`::method`
+
+函数名前的双冒号，`使函数变成了对象`。
+
+> 在 Kotlin 里，一个函数名的左边加上双冒号，它就不表示这个函数本身了，而表示一个对象，或者说一个指向对象的引用，但，这个对象可不是函数本身，而是一个和这个函数具有相同功能的对象。
+
+注意「函数」和「函数类型的对象」的区分。
+
+`::method` 的写法是指向一个对象的引用，而不是指向函数本身。这个对象是 Kotlin 自动创建的。
+
+##### 匿名函数
+```
+a(fun(param: Int): String {
+    return param.toString()
+})
+
+val f =  fun(param: Int): String {
+    val result = param * 10
+    return "$result"
+}
+```
+##### Lambda
+```
+setOnClickListener{ v: View ->
+    println(v)
+}
+```
+
+注意 Lambda 作为函数参数时的变形：简化写法。具体参见示例代码。
+
+Lambda 的最后一行代码就是它的返回值，而 return 会直接结束外层函数。
+
+###### 匿名函数和 Lambda 的本质
+
+在 Kotlin 里，匿名函数和 Lambda 的本质都是「函数类型的对象」。
